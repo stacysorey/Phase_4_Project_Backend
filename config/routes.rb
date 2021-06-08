1bf1fcc4ab89d8f62d1bad4e633711b760d34ed4
stacysorey <stacysorey@gmail.com>
2021-06-08 00:47:06 -0400

    update rails routes with name spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :entries
-  resources :journals
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :entries
+      resources :journals
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
