9c4bdad2878ee1875ffc8dcd9a44336132eb4cb2
stacysorey <stacysorey@gmail.com>
2021-06-08 01:11:13 -0400

    create journal entry seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,74 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# create_table "entries", force: :cascade do |t|
+#   t.string "title"
+#   t.text "description"
+#   t.date "date"
+#   t.integer "journal_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["journal_id"], name: "index_entries_on_journal_id"
+# end
+
 journal_1 = Journal.create(title: "Journal 1")
 journal_2 = Journal.create(title: "Journal 2")
 journal_3 = Journal.create(title: "Journal 3")
+
+journal_1.entries.build(
+  title: "Entry 1",
+  desciption: "This is the description for entry 1!",
+  date: Date.now
+)
+
+journal_1.entries.build(
+  title: "Entry 2",
+  desciption: "This is the description for entry 2!",
+  date: Date.now
+)
+
+journal_1.entries.build(
+  title: "Entry 3",
+  desciption: "This is the description for entry 3!",
+  date: Date.now
+)
+
+journal_2.entries.build(
+  title: "Entry 1",
+  desciption: "This is the description for entry 1!",
+  date: Date.now
+)
+
+journal_2.entries.build(
+  title: "Entry 2",
+  desciption: "This is the description for entry 2!",
+  date: Date.now
+)
+
+journal_2.entries.build(
+  title: "Entry 3",
+  desciption: "This is the description for entry 3!",
+  date: Date.now
+)
+
+journal_3.entries.build(
+  title: "Entry 1",
+  desciption: "This is the description for entry 1!",
+  date: Date.now
+)
+
+journal_3.entries.build(
+  title: "Entry 2",
+  desciption: "This is the description for entry 2!",
+  date: Date.now
+)
+
+journal_3.entries.build(
+  title: "Entry 3",
+  desciption: "This is the description for entry 3!",
+  date: Date.now
+)
+
+journal_1.save
+journal_2.save
+journal_3.save
