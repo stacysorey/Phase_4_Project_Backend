5132aed55ebcf3ed605da439f3e87263e4095d8f
stacysorey <stacysorey@gmail.com>
2021-06-18 05:14:46 -0400

    cnest entry route under journals

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :entries
-      resources :journals
+      resources :journals do
+        resources :entries
+      end
     end
   end
 
